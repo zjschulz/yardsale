42d33e0ada5df35d410d0268905fd940491fa4de
zjschulz <zjschulz@gmail.com>
2020-04-28 17:00:10 -0400

    Implementing the Registration Controller

--- yardsale-backend/app/controllers/sessions_controller.rb
+++ yardsale-backend/app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
     def create
       user = User
               .find_by(email: params["user"]["email"])
@@ -15,4 +16,5 @@ class SessionsController < ApplicationController
         render json: { status: 401 }
       end
     end
+    
   end
--- yardsale-backend/config/routes.rb
+++ yardsale-backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "static#home"
+  #static root made for testing purposes
   resources :items
   resources :shops
   resources :sessions, only: [:create]
+  resources :registrations, only [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

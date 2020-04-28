33836b6ae858a43d21a7490f03f0a4ceba7d6454
zjschulz <zjschulz@gmail.com>
2020-04-28 17:17:13 -0400

    more edits to test and correct authentication using registration controller

--- yardsale-backend/config/routes.rb
+++ yardsale-backend/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :items
   resources :shops
   resources :sessions, only: [:create]
-  resources :registrations, only [:create]
+  resources :registrations, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

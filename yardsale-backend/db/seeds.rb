33c207380acd471db24d4516db08797fd642529b
zjschulz <zjschulz@gmail.com>
2020-05-03 22:11:42 -0400

    updated styles

--- yardsale-backend/db/seeds.rb
+++ yardsale-backend/db/seeds.rb
@@ -9,6 +9,6 @@
 
 
 
-Item.create(name: "plant",description: "Green living thing",image_url: "https://img-s3.onedio.com/id-591375b7088340160f753f02/rev-0/raw/s-ed2c84670cd15a0ce0fa2833be72bce243431fca.jpg",price: 3.50,user_id: 1)
-Item.create(name: "chair",description: "Place to sit down",image_url: "https://tse2.mm.bing.net/th?id=OIP.tIykSwi6KsCAryCEgIdYXwHaHa&pid=Api&P=0&w=300&h=300",price: 15.00,user_id: 1)
-Item.create(name: "game",description: "Test of love",image_url: "http://c.shld.net/rpx/i/s/i/spin/10000351/prod_12303494912??hei=64&wid=64&qlt=50",price: 45.00,user_id: 1)
+Item.create(name: "Plant",description: "Green living thing",image_url: "https://img-s3.onedio.com/id-591375b7088340160f753f02/rev-0/raw/s-ed2c84670cd15a0ce0fa2833be72bce243431fca.jpg",price: 3.50,user_id: 1)
+Item.create(name: "Chair",description: "Place to sit down",image_url: "https://tse2.mm.bing.net/th?id=OIP.tIykSwi6KsCAryCEgIdYXwHaHa&pid=Api&P=0&w=300&h=300",price: 15.00,user_id: 1)
+Item.create(name: "Game",description: "Test of love",image_url: "http://c.shld.net/rpx/i/s/i/spin/10000351/prod_12303494912??hei=64&wid=64&qlt=50",price: 45.00,user_id: 1)

8ee1867da18cffc2bb8487eb7a3baba5cc9549df
zjschulz <zjschulz@gmail.com>
2020-05-04 15:04:38 -0400

    renderItems function expanded upon

--- yardsale-frontend/src/appContainer.js
+++ yardsale-frontend/src/appContainer.js
@@ -25,12 +25,43 @@ class AppContainer {
 
     //create DOM elements
     renderItems() {
-        const ul = document.createElement('ul');
         AppContainer.items.forEach(item => {
-            const li = document.createElement('li');
-            li.innerText = item.name;
-            ul.appendChild(li);
+        // Create elements needed to build a card  
+        const div = document.createElement('div')
+        const div1 = document.createElement('div')
+        const h4 = document.createElement('h4')
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        const p1 = document.createElement('p')
+        const btn = document.createElement('button')
+        const str = document.createElement('strong')
+
+        // Append newly created elements into the DOM
+        const body = document.querySelector('body');
+        body.append(div)
+        div.append(div1)
+        div1.append(h4)
+        div1.append(img)
+        div1.append(p)
+        div1.append(str)
+        str.append(p1)
+        div1.append(btn)
+
+        // Set content and attributes
+        div.setAttribute('class',"card text-white bg-success mb-3")
+        div.setAttribute('style',"max-width: 20rem;")
+        div1.setAttribute('class',"card-body")
+        h4.setAttribute('class','card-title')
+        h4.innerHTML = item.name
+        img.setAttribute('src', item.image_url)
+        img.setAttribute('style',"height: 200px; width: 100%; display: block;")
+        p.setAttribute('class',"card-text")
+        p.innerHTML = item.description
+        p1.setAttribute('class',"card-text")
+        p1.innerHTML = item.price
+        btn.setAttribute('type',"button")
+        btn.setAttribute('class',"btn btn-primary btn-sm")
+        btn.innerHTML = "Email Seller"
         })
-        document.body.appendChild(ul)
     };
 }
